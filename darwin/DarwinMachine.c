53f1e81dce6691c7a111339bdf3dafeb2a5b5fb2
SuCicada <pengyifu@gmail.com>
2024-04-10 10:42:10 +0900

    darwin: Enhance memory metrics support for Apple Silicon (ARM64)

--- darwin/DarwinMachine.c
+++ darwin/DarwinMachine.c
@@ -59,23 +59,19 @@ static unsigned DarwinMachine_allocateCPULoadInfo(processor_cpu_load_info_t* p) 
    return cpu_count;
 }
 
-static void DarwinMachine_getVMStats(vm_statistics_t p) {
-   mach_msg_type_number_t info_size = HOST_VM_INFO_COUNT;
-
-   if (host_statistics(mach_host_self(), HOST_VM_INFO, (host_info_t)p, &info_size) != 0) {
-      CRT_fatalError("Unable to retrieve VM statistics");
-   }
-}
-
-#if defined(__arm64__)
-static void DarwinMachine_getVMStats64(vm_statistics64_t p) {
+static void DarwinMachine_getVMStats(DarwinMachine* this) {
+#if defined(__LP64__)
    mach_msg_type_number_t info_size = HOST_VM_INFO64_COUNT;
 
-   if (host_statistics64(mach_host_self(), HOST_VM_INFO64, (host_info_t) p, &info_size) != 0) {
+   if (host_statistics64(mach_host_self(), HOST_VM_INFO64, (host_info_t)&this->vm_stats64, &info_size) != 0) {
+#else
+   mach_msg_type_number_t info_size = HOST_VM_INFO_COUNT;
+
+   if (host_statistics(mach_host_self(), HOST_VM_INFO, (host_info_t)&this->vm_stats, &info_size) != 0) {
+#endif
       CRT_fatalError("Unable to retrieve VM statistics64");
    }
 }
-#endif
 
 void Machine_scan(Machine* super) {
    DarwinMachine* host = (DarwinMachine*) super;
@@ -84,10 +80,7 @@ void Machine_scan(Machine* super) {
    DarwinMachine_freeCPULoadInfo(&host->prev_load);
    host->prev_load = host->curr_load;
    DarwinMachine_allocateCPULoadInfo(&host->curr_load);
-   DarwinMachine_getVMStats(&host->vm_stats);
-#if defined(__arm64__)
-   DarwinMachine_getVMStats64(&host->vm_stats64);
-#endif
+   DarwinMachine_getVMStats(host);
    openzfs_sysctl_updateArcStats(&host->zfs);
 }
 
@@ -104,10 +97,7 @@ Machine* Machine_new(UsersTable* usersTable, uid_t userId) {
    DarwinMachine_allocateCPULoadInfo(&this->curr_load);
 
    /* Initialize the VM statistics */
-   DarwinMachine_getVMStats(&this->vm_stats);
-#if defined(__arm64__)
-   DarwinMachine_getVMStats64(&this->vm_stats64);
-#endif
+   DarwinMachine_getVMStats(this);
 
    /* Initialize the ZFS kstats, if zfs.kext loaded */
    openzfs_sysctl_init(&this->zfs);
--- darwin/DarwinMachine.h
+++ darwin/DarwinMachine.h
@@ -18,9 +18,10 @@ typedef struct DarwinMachine_ {
    Machine super;
 
    host_basic_info_data_t host_info;
-   vm_statistics_data_t vm_stats;
-#if defined(__arm64__)
+#if defined(__LP64__)
    vm_statistics64_data_t vm_stats64;
+#else
+   vm_statistics_data_t vm_stats;
 #endif
    processor_cpu_load_info_t prev_load;
    processor_cpu_load_info_t curr_load;
